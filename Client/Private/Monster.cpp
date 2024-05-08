899721697741050f1fb5a1bb06be385dc643e231
JinYoung <tick5513@naver.com>
2024-05-08 16:27:11 +0900

    Monster Class 생성

---
+++ Client/Private/Monster.cpp
@@ -0,0 +1,39 @@
+#include "stdafx.h"
+
+#include "Monster.h"
+
+CMonster::CMonster(LPDIRECT3DDEVICE9 _pGraphicDevice)
+	:CGameObject{ _pGraphicDevice }
+{
+}
+
+CMonster::CMonster(const CMonster& Prototype)
+	:CGameObject{ Prototype }
+{
+}
+
+HRESULT CMonster::Initialize_Prototype()
+{
+	return S_OK;
+}
+
+HRESULT CMonster::Initialize(void* pArg)
+{
+	return S_OK;
+}
+
+HRESULT CMonster::Ready_Component()
+{
+	return S_OK;
+}
+
+HRESULT CMonster::Load_Texture()
+{
+	return S_OK;
+}
+
+void CMonster::Free()
+{
+	__super::Free();
+
+}
--- Client/Public/Monster.h
+++ Client/Public/Monster.h
@@ -1,12 +1,14 @@
 #pragma once
 
-#include "Client.h"
+#include "Client_Defines.h"
 #include "GameObject.h"
 
 BEGIN(Engine)
+
 class CTexture;
-class CTransform;
 class CVIBuffer_Rect;
+class CTransForm;
+
 END
 
 BEGIN(Client)
@@ -14,8 +16,30 @@ BEGIN(Client)
 class CMonster abstract : public CGameObject
 {
 protected:
+	enum MON_STATE { IDLE, WALK, ATTACK };
+public:
+	CMonster(LPDIRECT3DDEVICE9 _pGraphicDevice);
+	CMonster(const CMonster& Prototype);
+	virtual ~CMonster() = default;
 
+public:
+	virtual HRESULT Initialize_Prototype()					override;
+	virtual HRESULT Initialize(void* pArg)					override;
+
+public:
+	virtual HRESULT Ready_Component();
+	virtual HRESULT Load_Texture();
+
+protected:
+	CTexture* m_pTextureCom = { nullptr };
+	CVIBuffer_Rect* m_pVIBufferCom = { nullptr };
+	CTransForm* m_pTransformCom = { nullptr };
+
+	MON_STATE m_eMon_State;
+
+public:
+	virtual CGameObject* Clone(void* pArg = nullptr) = 0;
+	virtual void Free() override;
 };
 
 END
-
