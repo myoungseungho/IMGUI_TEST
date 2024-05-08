163acae7207d2c7701ebd5c4c808d6d89b471fcb
JinYoung <tick5513@naver.com>
2024-05-09 01:46:15 +0900

    타이머 에러 해결

--- Client/Public/Skill_Monster.h
+++ Client/Public/Skill_Monster.h
@@ -8,6 +8,7 @@ BEGIN(Engine)
 class CTexture;
 class CVIBuffer_Rect;
 class CTransform;
+class CCalc_Timer;
 
 END
 
@@ -26,10 +27,12 @@ public:
 
 public:
 	virtual HRESULT Ready_Components();
+	
 protected:
 	CTexture* m_pTextureCom = { nullptr };
 	CVIBuffer_Rect* m_pVIBufferCom = { nullptr };
 	CTransform* m_pTransformCom = { nullptr };
+	CCalc_Timer* m_pTimerCom = { nullptr };
 
 public:
 	virtual CGameObject* Clone(void* pArg = nullptr) = 0;
