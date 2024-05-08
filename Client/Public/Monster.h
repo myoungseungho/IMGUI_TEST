b02bb88c151bece5c044af6c89fde79b669d4474
JinYoung <tick5513@naver.com>
2024-05-08 12:35:21 +0900

    Com

---
+++ Client/Public/Monster.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "Client.h"
+#include "GameObject.h"
+
+BEGIN(Engine)
+class CTexture;
+class CTransform;
+class CVIBuffer_Rect;
+END
+
+BEGIN(Client)
+
+class CMonster abstract : public CGameObject
+{
+protected:
+
+};
+
+END
+
