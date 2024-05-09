5837a7b76c682ff7f2f9b7093c00cdb595c3604d
MYOUNGSEUNGHO <myoung6458@naver.com>
2024-05-09 12:50:46 +0900

    FileManager SAVE,LOAD,PARSE 함수 만듬 (안정성 검사 X)

--- Engine/Private/FileManager.cpp
+++ Engine/Private/FileManager.cpp
@@ -1,5 +1,5 @@
 #include "..\Public\FileManager.h"
-
+#include <sstream>
 CFileManager::CFileManager()
 {
 }
@@ -7,6 +7,92 @@ CFileManager::CFileManager()
 HRESULT CFileManager::Initialize()
 {
 	return S_OK;
+}
+
+HRESULT CFileManager::SaveObjects(const wstring& filename)
+{
+	wofstream file(filename);
+	if (!file.is_open()) {
+		throw std::runtime_error("Cannot open file for writing.");
+		return E_FAIL;
+	}
+
+	for (const auto& data : m_vecFileData) {
+		file << L"LayerName: " << data.layerName << L"\n";
+		file << L"PrototypeTag: " << data.prototypeTag << L"\n";
+		file << L"LevelIndex: " << data.levelIndex << L"\n";
+		file << L"Position: " << data.position.x << L" " << data.position.y << L" " << data.position.z << L"\n";
+		file << L"Rotation: " << data.rotation.x << L" " << data.rotation.y << L" " << data.rotation.z << L"\n";
+		file << L"Scale: " << data.scale.x << L" " << data.scale.y << L" " << data.scale.z << L"\n\n";
+	}
+
+	file.close();
+
+	return S_OK;
+}
+
+HRESULT CFileManager::LoadObjects(const wstring& filename)
+{
+	std::wifstream file(filename);
+	if (!file.is_open()) {
+		throw std::runtime_error("Cannot open file for reading.");
+		return E_FAIL;
+	}
+
+	wstring line;
+	FileData data;
+
+	while (getline(file, line)) {
+		ParseLine(line, data);
+		if (line.empty()) {
+			m_vecFileData.push_back(data);  // Assuming a blank line indicates the end of one object's data
+		}
+	}
+
+	file.close();
+
+	return S_OK;
+}
+
+HRESULT CFileManager::ParseLine(const wstring& line, FileData& obj)
+{
+	wistringstream iss(line);
+	wstring key;
+	if (getline(iss, key, L':')) {
+		wstring value;
+		getline(iss, value);
+		if (key == L"LayerName") {
+			obj.layerName = value;
+		}
+		else if (key == L"PrototypeTag") {
+			obj.prototypeTag = value;
+		}
+		else if (key == L"LevelIndex") {
+			obj.levelIndex = std::stoi(value);
+		}
+		else if (key == L"Position" || key == L"Rotation" || key == L"Scale") {
+			std::wistringstream vals(value);
+			float a, b, c;
+			vals >> a >> b >> c;
+			if (key == L"Position") {
+				obj.position.x = a;
+				obj.position.y = b;
+				obj.position.z = c;
+			}
+			else if (key == L"Rotation") {
+				obj.rotation.x = a;
+				obj.rotation.y = b;
+				obj.rotation.z = c;
+			}
+			else if (key == L"Scale") {
+				obj.scale.x = a;
+				obj.scale.y = b;
+				obj.scale.z = c;
+			}
+		}
+	}
+	else
+		return E_FAIL;
 }
 
 CFileManager* CFileManager::Create()
--- Engine/Public/FileManager.h
+++ Engine/Public/FileManager.h
@@ -11,11 +11,26 @@ BEGIN(Engine)
 #pragma once
 class CFileManager final : public CBase
 {
+	struct FileData
+	{
+		wstring layerName;
+		wstring prototypeTag;
+		_uint levelIndex;
+		_float3 position;
+		_float3 rotation;
+		_float3 scale;
+	};
 private:
 	CFileManager();
 	virtual ~CFileManager() = default;
 public:
 	HRESULT Initialize();
+public:
+	HRESULT SaveObjects(const wstring& filename);
+	HRESULT LoadObjects(const wstring& filename);
+	HRESULT ParseLine(const wstring& line, FileData& obj);
+private:
+	vector<FileData> m_vecFileData;
 public:
 	static CFileManager* Create();
 	virtual void Free() override;
