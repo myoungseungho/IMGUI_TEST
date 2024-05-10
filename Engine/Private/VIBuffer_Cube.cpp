e3edcb35062fbdc11859b959e4e0e33259e1b7fa
JinYoung <tick5513@naver.com>
2024-05-10 15:55:53 +0900

    VIBuffer_Cube 클래스 완성

--- Engine/Private/VIBuffer_Cube.cpp
+++ Engine/Private/VIBuffer_Cube.cpp
@@ -44,21 +44,83 @@ HRESULT CVIBuffer_Cube::Initialize_Prototype()
 	pVertices[3].vPosition = _float3(-0.5f, -0.5f, -0.5f);
 	pVertices[3].vTexcoord = _float2(0.0f, 0.f);
 
-	pVertices[0].vPosition = _float3(-0.5f, 0.5f, 0.5f);
-	pVertices[0].vTexcoord = _float2(0.0f, 0.f);
+	pVertices[4].vPosition = _float3(-0.5f, 0.5f, 0.5f);
+	pVertices[4].vTexcoord = _float2(0.0f, 0.f);
 
-	pVertices[1].vPosition = _float3(0.5f, 0.5f, 0.5f);
-	pVertices[1].vTexcoord = _float2(1.f, 0.f);
+	pVertices[5].vPosition = _float3(0.5f, 0.5f, 0.5f);
+	pVertices[5].vTexcoord = _float2(1.f, 0.f);
 
-	pVertices[2].vPosition = _float3(0.5f, 0.5f, -0.5f);
-	pVertices[2].vTexcoord = _float2(1.f, 1.f);
+	pVertices[6].vPosition = _float3(0.5f, 0.5f, -0.5f);
+	pVertices[6].vTexcoord = _float2(1.f, 1.f);
 
-	pVertices[3].vPosition = _float3(-0.5f, 0.5f, -0.5f);
-	pVertices[3].vTexcoord = _float2(0.0f, 0.f);
+	pVertices[7].vPosition = _float3(-0.5f, 0.5f, -0.5f);
+	pVertices[7].vTexcoord = _float2(0.0f, 0.f);
 
 	m_pVB->Unlock();
 
+	if (FAILED(m_pGraphic_Device->CreateIndexBuffer(m_iIndexStride * m_iNumIndices
+		, 0, m_eIndexFormat, D3DPOOL_MANAGED, &m_pIB, nullptr)))
+		return E_FAIL;
 
+	_ushort* pIndices = { nullptr };
+
+	m_pIB->Lock(0, 0, (void**)&pIndices, 0);
+
+	//¾Æ·§
+	pIndices[0] = 1;
+	pIndices[1] = 0;
+	pIndices[2] = 3;
+
+	pIndices[3] = 1;
+	pIndices[4] = 3;
+	pIndices[5] = 2;
+
+	//À§
+	pIndices[6] = 4;
+	pIndices[7] = 5;
+	pIndices[8] = 6;
+
+	pIndices[9] = 4;
+	pIndices[10] = 6;
+	pIndices[11] = 7;
+
+	//µÞ
+	pIndices[12] = 5;
+	pIndices[13] = 4;
+	pIndices[14] = 0;
+
+	pIndices[15] = 5;
+	pIndices[16] = 0;
+	pIndices[17] = 1;
+
+	//¿À¸¥ÂÊ
+	pIndices[18] = 6;
+	pIndices[19] = 5;
+	pIndices[20] = 1;
+
+	pIndices[21] = 6;
+	pIndices[22] = 1;
+	pIndices[23] = 2;
+
+	//¿ÞÂÊ
+	pIndices[24] = 7;
+	pIndices[25] = 4;
+	pIndices[26] = 0;
+
+	pIndices[27] = 7;
+	pIndices[28] = 0;
+	pIndices[29] = 3;
+
+	//¾Õ¸é
+	pIndices[30] = 7;
+	pIndices[31] = 6;
+	pIndices[32] = 2;
+
+	pIndices[33] = 7;
+	pIndices[34] = 2;
+	pIndices[35] = 3;
+
+	m_pIB->Unlock();
 
 
 	return S_OK;
