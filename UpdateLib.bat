9f01ee13ac37c908b4bcf19423c2eaf82bc9bba9
MYOUNGSEUNGHO <myoung6458@naver.com>
2024-05-16 15:57:18 +0900

    GitIgnore 추가

--- UpdateLib.bat
+++
@@ -1,7 +0,0 @@
-
-
-xcopy /옵션 .복사할파일이있는경로 .복사받을경로
-
-xcopy /y .\Engine\Public\*.h		.\EngineSDK\Inc\
-xcopy /y .\Engine\Bin\Engine.lib	.\EngineSDK\lib\
-xcopy /y .\Engine\Bin\Engine.dll	.\Client\bin\
