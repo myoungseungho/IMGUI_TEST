b93667d5def57c7ac8587db0def61fbbfb7ca511
MYOUNGSEUNGHO <myoung6458@naver.com>
2024-05-16 21:04:04 +0900

    Push_Stone 클래스 생성

---
+++ Client/Private/Push_Stone.cpp
@@ -0,0 +1,155 @@
+#include "stdafx.h"
+#include "..\Public\Push_Stone.h"
+
+#include "GameInstance.h"
+
+CPush_Stone::CPush_Stone(LPDIRECT3DDEVICE9 pGraphic_Device)
+	: CGameObject{ pGraphic_Device }
+{
+}
+
+CPush_Stone::CPush_Stone(const CPush_Stone& Prototype)
+	: CGameObject{ Prototype }
+{
+}
+
+HRESULT CPush_Stone::Initialize_Prototype()
+{
+	/* 원형객체의 초기화작업을 수행한다. */
+	/* 서버로부터 데이터를 받아오거나. 파일 입출력을 통해 데이터를 셋한다.  */
+
+	return S_OK;
+}
+
+HRESULT CPush_Stone::Initialize(void* pArg)
+{
+	if (FAILED(__super::Initialize(pArg)))
+		return E_FAIL;
+
+	if (FAILED(Ready_Components()))
+		return E_FAIL;
+
+	if (m_IsPasingObject)
+	{
+		FILEDATA* fileData = static_cast<FILEDATA*>(pArg);
+		m_pTransformCom->Set_Scaled(_float3(fileData->scale.x, fileData->scale.y, fileData->scale.z));
+		m_pTransformCom->Set_State(CTransform::STATE_POSITION, &_float3(fileData->position.x, fileData->position.y, fileData->position.z));
+	}
+	else
+	{
+		m_pTransformCom->Set_Scaled(_float3(1.0f, 1.0f, 1.f));
+		m_pTransformCom->Set_State(CTransform::STATE_POSITION, &_float3(3.0f, 0.f, 0.f));
+	}
+
+
+	/* For.Com_Transform */
+	CCollider::COLLIDER_DESC			ColliderDesc{};
+	ColliderDesc.center = m_pTransformCom->Get_State(CTransform::STATE_POSITION);
+	ColliderDesc.width = m_pTransformCom->Get_Scaled().x;
+	ColliderDesc.height = m_pTransformCom->Get_Scaled().y;
+	ColliderDesc.depth = 1.f;
+	ColliderDesc.MineGameObject = this;
+
+	//콜라이더 사본을 만들때 Cube 정보 추가해줘야 함.
+	if (FAILED(__super::Add_Component(LEVEL_GAMEPLAY, TEXT("Prototype_Component_Collider"),
+		TEXT("Com_Collider"), reinterpret_cast<CComponent**>(&m_pColliderCom), &ColliderDesc)))
+		return E_FAIL;
+
+	//콜라이더오브젝트 추가
+	m_pGameInstance->Add_ColliderObject(CCollider_Manager::CG_STATIC, this);
+
+	return S_OK;
+}
+
+void CPush_Stone::Priority_Update(_float fTimeDelta)
+{
+}
+
+void CPush_Stone::Update(_float fTimeDelta)
+{
+}
+
+void CPush_Stone::Late_Update(_float fTimeDelta)
+{
+	m_pGameInstance->Add_RenderObject(CRenderer::RG_NONBLEND, this);
+}
+
+HRESULT CPush_Stone::Render()
+{
+	m_pGraphic_Device->SetRenderState(D3DRS_CULLMODE, D3DCULL_NONE);
+
+	/* 사각형위에 올리고 싶은 테긋쳐를 미리 장치에 바인딩한다.  */
+	if (FAILED(m_pTextureCom->Bind_Texture(0)))
+		return E_FAIL;
+
+	if (FAILED(m_pTransformCom->Bind_WorldMatrix()))
+		return E_FAIL;
+
+	if (FAILED(m_pVIBufferCom->Render()))
+		return E_FAIL;
+
+	m_pGraphic_Device->SetRenderState(D3DRS_CULLMODE, D3DCULL_CCW);
+
+	return S_OK;
+}
+
+HRESULT CPush_Stone::Ready_Components()
+{
+	/* For.Com_Texture */
+	if (FAILED(__super::Add_Component(LEVEL_GAMEPLAY, TEXT("Prototype_Component_Texture_Tree"),
+		TEXT("Com_Texture"), reinterpret_cast<CComponent**>(&m_pTextureCom))))
+		return E_FAIL;
+
+	/* For.Com_VIBuffer */
+	if (FAILED(__super::Add_Component(LEVEL_STATIC, TEXT("Prototype_Component_VIBuffer_Rect"),
+		TEXT("Com_VIBuffer"), reinterpret_cast<CComponent**>(&m_pVIBufferCom))))
+		return E_FAIL;
+
+	/* For.Com_Transform */
+	CTransform::TRANSFORM_DESC			TransformDesc{};
+	TransformDesc.fSpeedPerSec = 1.0f;
+	TransformDesc.fRotationPerSec = D3DXToRadian(90.0f);
+
+	if (FAILED(__super::Add_Component(LEVEL_STATIC, TEXT("Prototype_Component_Transform"),
+		TEXT("Com_Transform"), reinterpret_cast<CComponent**>(&m_pTransformCom), &TransformDesc)))
+		return E_FAIL;
+
+	return S_OK;
+}
+
+CPush_Stone* CPush_Stone::Create(LPDIRECT3DDEVICE9 pGraphic_Device)
+{
+	CPush_Stone* pInstance = new CPush_Stone(pGraphic_Device);
+
+	if (FAILED(pInstance->Initialize_Prototype()))
+	{
+		MSG_BOX(TEXT("Failed to Created : CTree"));
+		Safe_Release(pInstance);
+	}
+
+	return pInstance;
+}
+
+
+CGameObject* CPush_Stone::Clone(void* pArg)
+{
+	CPush_Stone* pInstance = new CPush_Stone(*this);
+
+	if (FAILED(pInstance->Initialize(pArg)))
+	{
+		MSG_BOX(TEXT("Failed to Cloned : CPush_Stone"));
+		Safe_Release(pInstance);
+	}
+
+	return pInstance;
+}
+
+void CPush_Stone::Free()
+{
+	__super::Free();
+
+	Safe_Release(m_pTransformCom);
+	Safe_Release(m_pVIBufferCom);
+	Safe_Release(m_pTextureCom);
+	Safe_Release(m_pColliderCom);
+}
---
+++ Client/Public/Push_Stone.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "Client_Defines.h"
+#include "GameObject.h"
+
+
+BEGIN(Engine)
+class CTexture;
+class CTransform;
+class CVIBuffer_Rect;
+class CCollider;
+END
+
+BEGIN(Client)
+
+class CPush_Stone final : public CGameObject
+{	
+private:
+	CPush_Stone(LPDIRECT3DDEVICE9 pGraphic_Device); /* 원형생성 시 */
+	CPush_Stone(const CPush_Stone& Prototype); /* 사본생성 시 */
+	virtual ~CPush_Stone() = default;
+
+public:
+	virtual HRESULT Initialize_Prototype() override;
+	virtual HRESULT Initialize(void* pArg) override;
+	virtual void Priority_Update(_float fTimeDelta) override;
+	virtual void Update(_float fTimeDelta) override;
+	virtual void Late_Update(_float fTimeDelta) override;
+	virtual HRESULT Render() override;
+
+private:	
+	CTexture*			m_pTextureCom = { nullptr };
+	CTransform*			m_pTransformCom = { nullptr };
+	CVIBuffer_Rect*		m_pVIBufferCom = { nullptr };
+	CCollider* m_pColliderCom = { nullptr };
+
+private:
+	HRESULT Ready_Components();
+
+public:
+	/* 원형객체를 생성한다. */
+	static CPush_Stone* Create(LPDIRECT3DDEVICE9 pGraphic_Device);
+
+	/* 원형객체를 복제한 사본객체를 생성한다.(내 게임내에서 실제 동작하기위한 객체들) */
+	virtual CGameObject* Clone(void* pArg = nullptr ) override;
+	virtual void Free() override;
+};
+
+END
