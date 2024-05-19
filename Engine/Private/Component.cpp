f9edd323f03bfc15c93754522b4bba361828d59c
DESKTOP-4ED1A3K\명승호 <myoung6458@naver.com>
2024-05-19 22:47:50 +0900

    피킹구현 성공

--- Engine/Private/Component.cpp
+++ Engine/Private/Component.cpp
@@ -4,18 +4,22 @@
 CComponent::CComponent(LPDIRECT3DDEVICE9 pGraphic_Device)
 	: m_pGraphic_Device{ pGraphic_Device }
 	, m_isCloned{ false }
+	, m_pGameInstance{ CGameInstance::Get_Instance() }
+
 {
+	Safe_AddRef(m_pGameInstance);
 	Safe_AddRef(m_pGraphic_Device);
 }
 
-CComponent::CComponent(const CComponent & Prototype)
+CComponent::CComponent(const CComponent& Prototype)
 	: m_pGraphic_Device{ Prototype.m_pGraphic_Device }
 	, m_isCloned{ true }
+	, m_pGameInstance{ Prototype.m_pGameInstance }
 
 {
+	Safe_AddRef(m_pGameInstance);
 	Safe_AddRef(m_pGraphic_Device);
 }
-
 HRESULT CComponent::Initialize_Prototype()
 {
 	return S_OK;
@@ -31,5 +35,6 @@ void CComponent::Free()
 {
 	__super::Free();
 
+	Safe_Release(m_pGameInstance);
 	Safe_Release(m_pGraphic_Device);
 }
