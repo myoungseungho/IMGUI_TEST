585ba5bcbb6a805b25aca327e25875c4a4d6b781
MYOUNGSEUNGHO <myoung6458@naver.com>
2024-05-20 22:08:54 +0900

    IMGUI 복사 기능 구현 성공

--- Client/Private/EnviormentObject.cpp
+++ Client/Private/EnviormentObject.cpp
@@ -40,26 +40,45 @@ HRESULT CEnviormentObject::Initialize(void* pArg)
 {
 	__super::Initialize(pArg);
 
-	//카메라 트랜스폼
-	CComponent* camera_componenet = m_pGameInstance->Get_Component(LEVEL_TACHO, TEXT("Layer_Camera"), TEXT("Com_Transform"));
-	CTransform* camera_transform = static_cast<CTransform*>(camera_componenet);
+	if (pArg == nullptr)
+	{
+		//카메라 트랜스폼
+		CComponent* camera_componenet = m_pGameInstance->Get_Component(LEVEL_TACHO, TEXT("Layer_Camera"), TEXT("Com_Transform"));
+		CTransform* camera_transform = static_cast<CTransform*>(camera_componenet);
 
-	// 카메라의 현재 위치 가져오기
-	_float3 cameraPosition = camera_transform->Get_State(CTransform::STATE_POSITION);
+		// 카메라의 현재 위치 가져오기
+		_float3 cameraPosition = camera_transform->Get_State(CTransform::STATE_POSITION);
 
-	// 새로운 위치 계산 (Z 값은 카메라 위치에서 5만큼 떨어지고, Y는 0으로 설정)
-	float zOffset = 5.0f;
-	_float3 newPosition = cameraPosition;
-	newPosition.z += zOffset;
-	newPosition.y = 0.006f;
+		// 새로운 위치 계산 (Z 값은 카메라 위치에서 5만큼 떨어지고, Y는 0으로 설정)
+		float zOffset = 5.0f;
+		_float3 newPosition = cameraPosition;
+		newPosition.z += zOffset;
+		newPosition.y = 0.006f;
 
-	CComponent* componenet = Get_Component(TEXT("Com_Transform"));
-	CTransform* transform = static_cast<CTransform*>(componenet);
+		CComponent* componenet = Get_Component(TEXT("Com_Transform"));
+		CTransform* transform = static_cast<CTransform*>(componenet);
 
-	// 오브젝트의 위치 설정
-	transform->Set_State(CTransform::STATE_POSITION, &newPosition);
+		// 오브젝트의 위치 설정
+		transform->Set_State(CTransform::STATE_POSITION, &newPosition);
 
-	return S_OK;
+		return S_OK;
+	}
+	else
+	{
+		// pArg가 FILEDATA 타입인지 확인
+		FILEDATA* pFileData = reinterpret_cast<FILEDATA*>(pArg);
+		if (pFileData != nullptr && !pFileData->isParsing) // isParsing 값을 확인
+		{
+			// 전달된 위치와 스케일 적용
+			CComponent* component = Get_Component(TEXT("Com_Transform"));
+			CTransform* transform = static_cast<CTransform*>(component);
+
+			transform->Set_State(CTransform::STATE_POSITION, &pFileData->position);
+			transform->Set_Scaled(pFileData->scale);
+		}
+	}
+
+
 }
 
 void CEnviormentObject::Priority_Update(_float fTimeDelta)
