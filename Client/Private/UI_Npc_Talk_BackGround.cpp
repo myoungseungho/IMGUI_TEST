e124be08fd2ef6d6be9c762afb42dec931025d52
MYOUNGSEUNGHO <myoung6458@naver.com>
2024-05-27 17:13:34 +0900

    대화창은 띄웠음, 이제 덧댈 리소스 추가랑, 글자가 한글자씩 튀어나오게 만들어야함. 그리고 NPC랑 상호작용 시스템 만들어야함

--- Client/Private/UI_Npc_Talk_BackGround.cpp
+++ Client/Private/UI_Npc_Talk_BackGround.cpp
@@ -21,6 +21,37 @@ HRESULT CUI_Npc_Talk_BackGround::Initialize_Prototype()
 	return S_OK;
 }
 
+
+void CUI_Npc_Talk_BackGround::Font_Initialize()
+{
+	D3DXFONT_DESCW tFontInfo;
+	ZeroMemory(&tFontInfo, sizeof(D3DXFONT_DESCW));
+
+	// 폰트 설정 - m_pName_Font
+	tFontInfo.Height = 40;
+	tFontInfo.Width = 30;
+	tFontInfo.Weight = FW_HEAVY;
+	tFontInfo.CharSet = HANGEUL_CHARSET;
+	wcscpy_s(tFontInfo.FaceName, LF_FACESIZE, TEXT("Cafe24 Ssurround air OTF Light"));
+
+	if (FAILED(D3DXCreateFontIndirect(m_pGraphic_Device, &tFontInfo, &m_pName_Font)))
+	{
+		MSG_BOX(L"CreateFontIndirect for CurrentPlayerMoney_Font Failed");
+		return;
+	}
+
+	// 폰트 설정 - m_pTalk_Font
+	tFontInfo.Height = 30;
+	tFontInfo.Width = 20;
+	tFontInfo.Weight = FW_HEAVY;
+	// 이미 tFontInfo가 초기화되어 있으므로, 필요한 부분만 변경합니다.
+	if (FAILED(D3DXCreateFontIndirect(m_pGraphic_Device, &tFontInfo, &m_pTalk_Font)))
+	{
+		MSG_BOX(L"CreateFontIndirect for CurrentItemTitle_Font Failed");
+		return;
+	}
+}
+
 HRESULT CUI_Npc_Talk_BackGround::Initialize(void* pArg)
 {
 	if (FAILED(Ready_Components()))
@@ -29,16 +60,17 @@ HRESULT CUI_Npc_Talk_BackGround::Initialize(void* pArg)
 	if (FAILED(__super::Initialize(pArg)))
 		return E_FAIL;
 
-	m_fSizeX = 340.f;
-	m_fSizeY = 265.f;
-	m_fX = 450.f;
-	m_fY = -140.f;
+	m_fSizeX = 760.f;
+	m_fSizeY = 330.f;
+	m_fX = -15.f;
+	m_fY = -145.f;
 
 	m_pTransformCom->Set_Scaled(_float3(m_fSizeX, m_fSizeY, 1.f));
 	m_pTransformCom->Set_State(CTransform::STATE_POSITION, &_float3(m_fX, m_fY, 0.f));
 
+	Font_Initialize();
 
-	m_fAlpha = 66.f;
+	m_fAlpha = 190.f;
 	return S_OK;
 }
 
@@ -49,23 +81,83 @@ void CUI_Npc_Talk_BackGround::Priority_Update(_float fTimeDelta)
 
 void CUI_Npc_Talk_BackGround::Update(_float fTimeDelta)
 {
-	if (!m_bIsOn) return; // m_bIsOn이 false이면 업데이트를 수행하지 않음
-	
+	/*if (GetAsyncKeyState('F') & 0x8000) {
+		offsetX -= 5.f;
+	}
+	if (GetAsyncKeyState('H') & 0x8000) {
+		offsetX += 5.f;
+	}
+	if (GetAsyncKeyState('T') & 0x8000) {
+		offsetY += 5.f;
+	}
+	if (GetAsyncKeyState('G') & 0x8000) {
+		offsetY -= 5.f;
+	}
+	if (GetAsyncKeyState('R') & 0x8000) {
+		offsetZ -= 0.01f;
+	}
+	if (GetAsyncKeyState('Y') & 0x8000) {
+		offsetZ += 0.01f;
+	}
+	if (GetAsyncKeyState('J') & 0x8000) {
+		offsetXScale -= 5.f;
+	}
+	if (GetAsyncKeyState('K') & 0x8000) {
+		offsetXScale += 5.f;
+	}
+	if (GetAsyncKeyState('N') & 0x8000) {
+		offsetYScale -= 5.f;
+	}
+	if (GetAsyncKeyState('M') & 0x8000) {
+		offsetYScale += 5.f;
+	}
+	if (GetAsyncKeyState(VK_UP) & 0x8000) {
+		m_fAlpha += 1.f;
+	}
+	if (GetAsyncKeyState(VK_DOWN) & 0x8000) {
+		m_fAlpha -= 1.f;
+	}*/
+
+
+
+	// 키 입력 처리
+	if (GetAsyncKeyState(VK_UP) & 0x8000)
+	{
+		m_TextPosY -= 10.f; // 위쪽으로 이동
+	}
+	if (GetAsyncKeyState(VK_DOWN) & 0x8000)
+	{
+		m_TextPosY += 10.f; // 아래쪽으로 이동
+	}
+	if (GetAsyncKeyState(VK_LEFT) & 0x8000)
+	{
+		m_TextPosX -= 10.f; // 왼쪽으로 이동
+	}
+	if (GetAsyncKeyState(VK_RIGHT) & 0x8000)
+	{
+		m_TextPosX += 10.f; // 오른쪽으로 이동
+	}
 }
 
 void CUI_Npc_Talk_BackGround::Late_Update(_float fTimeDelta)
 {
-	if (!m_bIsOn) return; // m_bIsOn이 false이면 업데이트를 수행하지 않음
+	//if (!m_bIsOn) return; // m_bIsOn이 false이면 업데이트를 수행하지 않음
 	__super::Late_Update(fTimeDelta);
-
-	
+	//
+	//m_pTransformCom->Set_Scaled(_float3(m_fSizeX + offsetXScale, m_fSizeY + offsetYScale, 1.f));
+	//m_pTransformCom->Set_State(CTransform::STATE_POSITION, &_float3(m_fX + offsetX, m_fY + offsetY, 0.f));
 	m_pGameInstance->Add_RenderObject(CRenderer::RG_UI, this);
 }
 
 HRESULT CUI_Npc_Talk_BackGround::Render(_float fTimeDelta)
 {
-	if (!m_bIsOn) return S_OK; // m_bIsOn이 false이면 렌더링을 수행하지 않음
+	//if (!m_bIsOn) return S_OK; // m_bIsOn이 false이면 렌더링을 수행하지 않음
 	__super::Begin_RenderState();
+
+	// 텍스트 형식화 및 렌더링
+
+	wchar_t text[256];
+	RECT rect;
 
 	/* 사각형위에 올리고 싶은 테긋쳐를 미리 장치에 바인딩한다.  */
 	if (FAILED(m_pTextureCom->Bind_Texture(0)))
@@ -76,6 +168,30 @@ HRESULT CUI_Npc_Talk_BackGround::Render(_float fTimeDelta)
 
 	if (FAILED(m_pVIBufferCom->Render()))
 		return E_FAIL;
+
+	// 텍스트 렌더링 - 이름
+	swprintf_s(text, L"%s", m_WstringName.c_str());
+	SetRect(&rect, static_cast<int>(300.f), static_cast<int>(370.f), 0, 0); // 텍스트를 출력할 위치 변경
+	m_pName_Font->DrawText(
+		NULL,
+		text,
+		-1,
+		&rect,
+		DT_NOCLIP,
+		D3DCOLOR_ARGB(255, 255, 255, 255)
+	);
+
+	// 텍스트 렌더링 - CurrentItemExplain
+	swprintf_s(text, L"%s", m_WstringTalk.c_str());
+	SetRect(&rect, static_cast<int>(570.f), static_cast<int>(460.f), 0, 0); // 텍스트를 출력할 위치 변경
+	m_pTalk_Font->DrawText(
+		NULL,
+		text,
+		-1,
+		&rect,
+		DT_NOCLIP,
+		D3DCOLOR_ARGB(255, 255, 255, 255)
+	);
 
 	__super::End_RenderState();
 
@@ -105,6 +221,7 @@ HRESULT CUI_Npc_Talk_BackGround::Ready_Components()
 
 	return S_OK;
 }
+
 
 CUI_Npc_Talk_BackGround* CUI_Npc_Talk_BackGround::Create(LPDIRECT3DDEVICE9 pGraphic_Device)
 {
--- Client/Public/UI_Npc_Talk_BackGround.h
+++ Client/Public/UI_Npc_Talk_BackGround.h
@@ -15,6 +15,11 @@ BEGIN(Client)
 class CUI_Npc_Talk_BackGround final : public CUIObject
 {	
 private:
+	struct ItemInfo {
+		wstring name;
+		wstring talk;
+	};
+private:
 	CUI_Npc_Talk_BackGround(LPDIRECT3DDEVICE9 pGraphic_Device); /* 원형생성 시 */
 	CUI_Npc_Talk_BackGround(const CUI_Npc_Talk_BackGround& Prototype); /* 사본생성 시 */
 	virtual ~CUI_Npc_Talk_BackGround() = default;
@@ -31,10 +36,17 @@ private:
 	CTexture*			m_pTextureCom = { nullptr };
 	CTransform*			m_pTransformCom = { nullptr };
 	CVIBuffer_Rect*		m_pVIBufferCom = { nullptr };
+	LPD3DXFONT			m_pName_Font = { nullptr };
+	LPD3DXFONT			m_pTalk_Font = { nullptr };
 
 private:
 	HRESULT Ready_Components();
-
+	void Font_Initialize();
+private:
+	wstring m_WstringName = { TEXT("이름 테스트") };
+	wstring m_WstringTalk = { TEXT("토크 테스트") };
+	_float m_TextPosX = 50.0f; // 텍스트 x 좌표
+	_float m_TextPosY = 50.0f; // 텍스트 y 좌표
 public:
 	/* 원형객체를 생성한다. */
 	static CUI_Npc_Talk_BackGround* Create(LPDIRECT3DDEVICE9 pGraphic_Device);
