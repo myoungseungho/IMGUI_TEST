7997f13316d0bda4a1b81a7a6c4bb1be9049652e
JinYoung <tick5513@naver.com>
2024-05-28 23:53:26 +0900

    Bug_Pix

--- Client/Public/Effect_Koofu_Smoke.h
+++ Client/Public/Effect_Koofu_Smoke.h
@@ -28,7 +28,6 @@ public:
 	virtual void Late_Update(_float fTimeDelta) override;
 	virtual HRESULT Render(_float fTimeDelta) override;
 
-
 private:
 	virtual HRESULT Ready_Components() override;
 	virtual HRESULT Ready_Animation();
@@ -41,16 +40,6 @@ private:
 
 private:
 	_uint m_iSmokeNum = { 0 };
-	D3DMATERIAL9		MaterialDesc{};
-	D3DLIGHT9			LightDesc{};
-
-	D3DMATERIAL9 InitMtrl(D3DXCOLOR a, D3DXCOLOR d, D3DXCOLOR s, D3DXCOLOR e, float p);
-
-	const D3DMATERIAL9 WHITE_MTRL = InitMtrl(D3DCOLOR_XRGB(255, 255, 255), D3DCOLOR_XRGB(255, 255, 255), D3DCOLOR_XRGB(255, 255, 255), D3DCOLOR_XRGB(0, 0, 0), 2.0f);
-	const D3DMATERIAL9 RED_MTRL = InitMtrl(D3DCOLOR_XRGB(255, 0, 0), D3DCOLOR_XRGB(255, 0, 0), D3DCOLOR_XRGB(255, 0, 0), D3DCOLOR_XRGB(0, 0, 0), 2.0f);
-	const D3DMATERIAL9 GREEN_MTRL = InitMtrl(D3DCOLOR_XRGB(0, 255, 0), D3DCOLOR_XRGB(0, 255, 0), D3DCOLOR_XRGB(0, 255, 0), D3DCOLOR_XRGB(0, 0, 0), 2.0f);
-	const D3DMATERIAL9 BLUE_MTRL = InitMtrl(D3DCOLOR_XRGB(0, 0, 255), D3DCOLOR_XRGB(0, 0, 255), D3DCOLOR_XRGB(0, 0, 255), D3DCOLOR_XRGB(0, 0, 0), 2.0f);
-	const D3DMATERIAL9 YELLOW_MTRL = InitMtrl(D3DCOLOR_XRGB(255, 255, 0), D3DCOLOR_XRGB(255, 255, 0), D3DCOLOR_XRGB(255, 255, 0), D3DCOLOR_XRGB(0, 0, 0), 2.0f);
 
 public:
 	static CEffect_Koofu_Smoke* Create(LPDIRECT3DDEVICE9 pGraphic_Device);
