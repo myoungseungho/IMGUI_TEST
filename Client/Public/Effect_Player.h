18abb5a0f34159c60e0f56f0ec7e218d7c0050ef
LAPTOP-39J18DB3\user <intomydoor@gmail.com>
2024-05-30 18:43:35 +0900

    리소스 정리, 플레이어 평타 이펙트 추가 중

---
+++ Client/Public/Effect_Player.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include "Client_Defines.h"
+#include "Effect.h"
+
+
+BEGIN(Engine)
+class CTexture;
+class CTransform;
+class CVIBuffer_Rect;
+class CCollider;
+class CCalc_Timer;
+END
+
+BEGIN(Client)
+
+class CEffect_Item final : public CEffect
+{	
+public:
+typedef struct
+	{
+		CTransform* pTargetTransform = { nullptr };
+	}EFFECT_ITEM_DESC;
+
+private:
+	CEffect_Item(LPDIRECT3DDEVICE9 pGraphic_Device); /* 원형생성 시 */
+	CEffect_Item(const CEffect_Item& Prototype); /* 사본생성 시 */
+	virtual ~CEffect_Item() = default;
+
+public:
+	virtual HRESULT Initialize_Prototype() override;
+	virtual HRESULT Initialize(void* pArg) override;
+	virtual void Priority_Update(_float fTimeDelta) override;
+	virtual void Update(_float fTimeDelta) override;
+	virtual void Late_Update(_float fTimeDelta) override;
+	virtual HRESULT Render(_float fTimeDelta) override;
+
+private:	
+	CTexture*				m_pTextureCom = { nullptr };
+	CTransform*			m_pTransformCom = { nullptr };
+	CVIBuffer_Rect*		m_pVIBufferCom = { nullptr };
+	CCollider*				m_pColliderCom = { nullptr };
+	CCalc_Timer*			m_pTimerCom = { nullptr };
+
+	CTransform* m_pTargetTransform = { nullptr };
+private:
+	HRESULT Ready_Components();
+	
+private:
+
+	
+public:
+	/* 원형객체를 생성한다. */
+	static CEffect_Item* Create(LPDIRECT3DDEVICE9 pGraphic_Device);
+
+	/* 원형객체를 복제한 사본객체를 생성한다.(내 게임내에서 실제 동작하기위한 객체들) */
+	virtual CGameObject* Clone(void* pArg = nullptr ) override;
+	virtual void Free() override;
+};
+
+END
